4e2608f982794d3ef5ac336f66131bbcfbd9cabd
Charlie Dunford <73963633+charlierdelta@users.noreply.github.com>
2020-12-02 14:33:17 -0500

    Update Ship.lua

--- Asteroids/Ship.lua
+++ Asteroids/Ship.lua
@@ -6,7 +6,7 @@ Ship = Class{}
 
 --[[
     Ship takes an x and y for starting position as well as a radius to determine size.
-    Initializes heading to point right and dx and dy to zero so there is it does not move
+    Initializes heading to point right and dx and dy to zero so it does not move until player input.
 ]]
 function Ship:init(x, y, radius)
     self.x = x
@@ -17,8 +17,19 @@ function Ship:init(x, y, radius)
     self.dy = 0
 end
 
-function Ship:update(dt)
+function Ship:collides(asteroid)
+    if self.x > asteroid.x + asteroid.radius + self.radius or asteroid.x > self.x + asteroid.radius + self.radius then
+        return false
+    end
 
+    if self.y > asteroid.y + asteroid.radius + self.radius or asteroid.y > self.y + asteroid.radius + self.radius then
+        return false
+    end 
+
+    return true
+end
+
+function Ship:update(dt)
     if self.y < 0 and self.dy < 0 then
         self.y = VIRTUAL_HEIGHT - self.radius - 1
     end
@@ -38,14 +49,21 @@ function Ship:update(dt)
     self.heading = self.heading % (2 * math.pi)
     
     self.x = self.x + self.dx * dt
-
 end
+
+function Ship:reset(x, y)
+    self.x = x
+    self.y = y
+    self.heading = 0
+    self.dx = 0
+    self.dy = 0
+end    
 
 --[[
     Uses LÖVE2D's `circle` function, which takes draw mode, 
     positions, and radius for the circle. Adds a line to show
-    player where the 'thrust' will push the ship, which is 
-    drawn with the ship's position and the heading and is as long
+    player where the front of the ship is, which is drawn with 
+    the ship's position and the heading and is as long
     as the radius
 ]]
 function Ship:render()
