a286184f132b577fdfff9992ddb038979ce5716e
Charlie Dunford <73963633+charlierdelta@users.noreply.github.com>
2020-12-02 14:33:43 -0500

    Update Asteroid.lua

--- Asteroids/Asteroid.lua
+++ Asteroids/Asteroid.lua
@@ -17,37 +17,7 @@ function Asteroid:init(x, y, radius, stage)
     self.dx = math.random(2) == 1 and math.random(-100, -80) or math.random(80, 100)
 end
 
---[[
-    Expects a ship as an argument and returns true or false, depending
-    on whether their radii overlap.
-]]
-function Asteroid:collides(ship)
 
-    if self.x > ship.x + ship.radius + self.radius or ship.x > self.x + ship.radius + self.radius then
-        return false
-    end
-
-    if self.y > ship.y + ship.radius + self.radius or ship.y > self.y + ship.radius + self.radius then
-        return false
-    end 
-
-    return true
-end
-
---[[
-    Places the Asteroid in the middle of the screen, with an initial random velocity
-    on both axes.
-]]
-function Asteroid:reset()
-    self.x = VIRTUAL_WIDTH / 2 - 2
-    self.y = VIRTUAL_HEIGHT / 2 - 2
-    self.dy = math.random(2) == 1 and math.random(-100, -80) or math.random(80, 100)
-    self.dx = math.random(2) == 1 and math.random(-100, -80) or math.random(80, 100)
-end
-
---[[
-    Simply applies velocity to position, scaled by deltaTime.
-]]
 function Asteroid:update(dt)
     if self.y < 0 and self.dy < 0 then
         self.y = VIRTUAL_HEIGHT - self.radius
@@ -71,10 +41,6 @@ end
 
 function Asteroid:setDY(dy)
     self.dy = dy
-end
-
-function Asteroid:setRadius(r)
-    self.radius = r
 end
 
 function Asteroid:render()
